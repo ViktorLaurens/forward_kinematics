8b21d921cfaf952c8b1413ec91ba8359da805f6e
Viktor Laurens De Groote <viktor.degroote@gmail.com>
2024-12-10 12:38:18 +0100

    Added test for homogeneous trnsform calculation.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,3 +18,25 @@ def test_quaternion_to_rotation_matrix():
         [0,  0, 1]
     ])
     assert np.allclose(rotation_matrix, expected, atol=1e-3)
+
+def test_homogeneous_transform():
+    """
+    Test the homogeneous_transform method.
+    """
+    # Test parameters
+    a = 1.0
+    d = 0.5
+    alpha = np.pi / 2
+    theta = np.pi / 4
+
+    robot = Robot()
+    T = robot.homogeneous_transform(a, d, alpha, theta)
+
+    # Expected transformation matrix
+    expected = np.array([
+        [np.sqrt(2)/2, -np.sqrt(2)/2, 0, np.sqrt(2)/2],
+        [0, 0, 1, 0.5],
+        [-np.sqrt(2)/2, -np.sqrt(2)/2, 0, -np.sqrt(2)/2],
+        [0, 0, 0, 1]
+    ])
+    assert np.allclose(T, expected, atol=1e-3)
