f336fe311b8d12452eae667c77368043fbde62b2
Viktor Laurens De Groote <viktor.degroote@gmail.com>
2024-12-10 11:56:08 +0100

    Changed input in main.

--- src/fk/__main__.py
+++ src/fk/__main__.py
@@ -3,7 +3,7 @@ from .utils import Robot
 def main():
     """Main entry point for the program."""
     robot = Robot()
-    print(robot.calc_fk([0, 0, 0, 0]))
+    print(robot.calc_fk([0, 0, 0, 0.2]))
 
 if __name__ == "__main__":
     main()
