a7c2ec5efce4ef7d8aa7f5f5585d8c90518c81a8
Viktor Laurens De Groote <viktor.degroote@gmail.com>
2024-12-10 11:01:01 +0100

    Added function to compute ho;ogeneous transform.

--- src/fk/utils.py
+++ src/fk/utils.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+def homogeneous_transform(self, a, d, alpha, theta):
+    """ Compute the Denavit-Hartenberg transformation matrix """
+    ct = np.cos(theta)
+    st = np.sin(theta)
+    ca = np.cos(alpha)
+    sa = np.sin(alpha)
+    return np.array([
+        [ct, -st*ca, st*sa, a*ct],
+        [st, ct*ca, -ct*sa, a*st],
+        [0, sa, ca, d],
+        [0, 0, 0, 1]
+    ])
